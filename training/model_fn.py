068a200471d0ad9a4263c436b76b8e322b4f4ac0
Ian Chew <igac96@gmail.com>
2018-03-17 19:48:24 -0400

    Moved the model function to its own file and cleaned up the code a bit.

---
+++ training/model_fn.py
@@ -0,0 +1,77 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+from config import * #Configuration options, like HEIGHT or WIDTH.
+
+# Features is the input data, in a batch.
+# Labels is a 1D array of result vaules.
+def cnn_model_fn(features, labels, mode):
+	# Input Layer, combine the two images into one image with 6 channels.
+	input_layer = tf.reshape(features["x"], [-1, HEIGHT, WIDTH, 6])
+
+	conv1 = tf.layers.conv2d(
+		inputs = input_layer,
+		filters = CHANNELS[0],
+		kernel_size = [5,5],
+		#Padding = same means that output is same dimension as input (last is 32)
+		padding = "same",
+		activation = tf.nn.relu)
+	
+	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides = 2)
+
+	conv2 = tf.layers.conv2d(
+		inputs = pool1,
+		filters = CHANNELS[2],
+		kernel_size = [5,5],
+		#Padding = same means that output is same dimension as input (last is 32)
+		padding = "same",
+		activation = tf.nn.relu)
+
+	pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=2, strides=2)
+	
+	pool2_flat = tf.reshape(pool2, [-1, HEIGHT/2**LAYERS * WIDTH/2**LAYERS * CHANNELS[2]])
+	
+	dense = tf.layers.dense(inputs=pool2_flat, units=DENSE_NODES, activation=tf.nn.relu)
+
+	dropout = tf.layers.dropout(
+		inputs = dense,
+		rate = 0.4,
+		training = mode == tf.contrib.learn.ModeKeys.TRAIN
+	)
+	
+	logits = tf.layers.dense(inputs=dropout, units=10)
+	
+	predictions = {
+		"classes" : tf.argmax(input=logits, axis=1),
+		"probabilities" : tf.nn.softmax(logits, name="softmax_tensor")
+	}
+	
+#	if mode == tf.estimator.ModeKeys.PREDICT:
+	if mode == tf.contrib.learn.ModeKeys.INFER:
+		return tf.contrib.learn.ModelFnOps(mode=mode, predictions=predictions)
+	
+	onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+
+	loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+
+
+	if mode == tf.contrib.learn.ModeKeys.TRAIN:
+		# Create optimizer object with learning rate.
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+		train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+		
+		return tf.contrib.learn.ModelFnOps(mode=mode, loss=loss, train_op=train_op)
+	
+	# In EVAL mode
+	eval_metric_ops = {
+		"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
+	}
+	
+#	return tf.estimator.ModelFnOps(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+	return tf.contrib.learn.ModelFnOps(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops, predictions=predictions)
+
--- training/train.py
+++ training/train.py
@@ -2,86 +2,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# Imports
+# Library imports
 import numpy as np
 import tensorflow as tf
 
-from config import * #Configuration options, like HEIGHT or WIDTH.
-
-tf.estimator = tf.contrib.learn
+# Local file imports
+from model_fn import cnn_model_fn
 
 tf.logging.set_verbosity(tf.logging.INFO)
-
-# Our application logic will be added here
-
-# Features is the input data, in a batch.
-# Labels is a 1D array of result vaules.
-def cnn_model_fn(features, labels, mode):
-	# Input Layer, combine the two images into one image with 6 channels.
-	input_layer = tf.reshape(features["x"], [-1, HEIGHT, WIDTH, 6])
-
-	me_too_thanks = "same"
-
-	conv1 = tf.layers.conv2d(
-		inputs = input_layer,
-		filters = CHANNELS[0],
-		kernel_size = [5,5],
-		#Padding = same means that output is same dimension as input (last is 32)
-		padding = me_too_thanks,
-		activation = tf.nn.relu)
-	
-	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides = 2)
-
-	conv2 = tf.layers.conv2d(
-		inputs = pool1,
-		filters = CHANNELS[2],
-		kernel_size = [5,5],
-		#Padding = same means that output is same dimension as input (last is 32)
-		padding = me_too_thanks,
-		activation = tf.nn.relu)
-
-	pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=2, strides=2)
-	
-	pool2_flat = tf.reshape(pool2, [-1, HEIGHT/2**LAYERS * WIDTH/2**LAYERS * CHANNELS[2]])
-	
-	dense = tf.layers.dense(inputs=pool2_flat, units=DENSE_NODES, activation=tf.nn.relu)
-
-	dropout = tf.layers.dropout(
-		inputs = dense,
-		rate = 0.4,
-		training = mode == tf.estimator.ModeKeys.TRAIN
-	)
-	
-	logits = tf.layers.dense(inputs=dropout, units=10)
-	
-	predictions = {
-		"classes" : tf.argmax(input=logits, axis=1),
-		"probabilities" : tf.nn.softmax(logits, name="softmax_tensor")
-	}
-	
-#	if mode == tf.estimator.ModeKeys.PREDICT:
-	if mode == tf.estimator.ModeKeys.INFER:
-		return tf.estimator.ModelFnOps(mode=mode, predictions=predictions)
-	
-	onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
-
-	loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
-
-
-	if mode == tf.estimator.ModeKeys.TRAIN:
-		# Create optimizer object with learning rate.
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-		train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-		
-		return tf.estimator.ModelFnOps(mode=mode, loss=loss, train_op=train_op)
-	
-	# In EVAL mode
-	eval_metric_ops = {
-		"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
-	}
-	
-#	return tf.estimator.ModelFnOps(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-	return tf.estimator.ModelFnOps(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops, predictions=predictions)
 
 def main(unused_argv):
 	# Load the training and eval data.
@@ -94,7 +22,7 @@ def main(unused_argv):
 	#Allocate 90% of the GPU's memory.
 	config = tf.contrib.learn.RunConfig(gpu_memory_fraction=0.9)
 	
-	mnist_classifier = tf.estimator.Estimator(
+	mnist_classifier = tf.contrib.learn.Estimator(
 		model_fn = cnn_model_fn,
 		model_dir = "/tmp/mnist_convnet_model",
 		config = config
@@ -121,7 +49,7 @@ def main(unused_argv):
 	
 #	mnist_classifier.train(
 #		input_fn = train_input_fn,
-#		steps = 1500,
+#		steps = 20000,
 #		hooks = [logging_hook]
 #	)
 
