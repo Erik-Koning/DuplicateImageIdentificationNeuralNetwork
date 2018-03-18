0f6ceb49f393089f89e9d2fb14c9684888aebbea
Ian Chew <igac96@gmail.com>
2018-03-18 14:24:37 -0400

    Made the number of layers easily configurable, and added KERNEL_SIZE and POOL_SIZE to config.py.

--- training/config.py
+++ training/config.py
@@ -1,16 +1,22 @@
-#Dimensions of the image
+# Dimensions of the image
 HEIGHT=128
 WIDTH=128
 
-#Number of convolution/max-pool layers
+# Number of convolution/max-pool layers
 LAYERS = 2
 
-#Number of channels after each layer
+# Number of channels after each layer
 CHANNELS = [32, 64]
 
-#Number of nodes in the second-to-last fully connected layer
+# Size of the kernel in the convolution layers.
+KERNEL_SIZE = 5
+
+# Size of the filter used in the max pooling layers.
+POOL_SIZE = 2
+
+# Number of nodes in the second-to-last fully connected layer
 DENSE_NODES = 1024
 
-#Size of each batch, and the number of epochs to run.
+# Size of each batch, and the number of epochs to run.
 BATCH_SIZE = 64
 EPOCHS = 10
