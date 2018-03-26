58eb13f53f3f9c65984165cc4af2db87c3db252d
Ian Chew <igac96@gmail.com>
2018-03-26 12:20:19 -0400

    Added support for JPEG compression in preprocessing.

--- preprocessing/transformations.py
+++ preprocessing/transformations.py
@@ -11,3 +11,12 @@ def resize(image, lbound = 0.5, hbound = 2.0):
 	ratio = random.uniform(lbound, hbound)
 	# Need to cast height and width to an integer.
 	return cv2.resize(image, (int(height*ratio), int(width*ratio)))
+
+# Compress an image using JPEG in a very lossy way.
+# Quality is a number from 0 to 100, with higher numbers representing
+# higher quality.
+def compress(image, quality=10):
+	# Flag list to send to imencode.
+	flags = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
+	res, image = cv2.imencode('.jpg', image, flags)
+	return cv2.imdecode(image, 1)
